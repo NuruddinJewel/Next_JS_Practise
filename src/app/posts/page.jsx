ff110f2166e63a021b8c7bbeb1de7de057c73cba
Jewel <m.76jewel@gmail.com>
2026-04-19 00:08:02 +0600

    Data fetching Ways

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -1,9 +1,39 @@
 import React from 'react';
 
-const PostsPage = () => {
+// const getPosts = async () => {
+//     const res = fetch('https://jsonplaceholder.typicode.com/posts');
+//     return rest.json();
+// }
+
+// try--catch
+// const getPosts = async () => {
+//     try {
+//         const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+//         return res.json();
+//     }
+//     catch (err) {
+//         throw new Error('Failed to fetch posts');
+//     }
+// }
+
+const getPosts = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+    if (!res.ok) {
+        throw new Error('Failed to fetch posts')
+    }
+    return res.json();
+}
+
+
+
+const PostsPage = async () => {
+    // const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+    // const posts = await res.json();
+
+    const posts = await getPosts();
     return (
         <div>
-            <h2>Posts are coming soon</h2>
+            <h2>Posts are coming soon : {posts.length}</h2>
         </div>
     );
 };
