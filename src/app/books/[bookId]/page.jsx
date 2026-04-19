eaef35741e2676a5a8c62a0294dad948f14f8f0e
Jewel <m.76jewel@gmail.com>
2026-04-19 18:18:23 +0600

    Optimization Ways

--- src/app/books/[bookId]/page.jsx
+++ src/app/books/[bookId]/page.jsx
@@ -1,32 +1,7 @@
-// import React from 'react';
-
-// const BookDetailsPage = async ({ params }) => {
-//     const { bookId } = await params;
-//     const res = await fetch(`http://localhost:5000/books/${bookId}`);
-//     const { title, author, price } = await res.json();
-//     return (
-//         <div>
-//             <h2>Book Details : {bookId} </h2>
-//             <h3>{title}</h3>
-//             <p>Author: {author} </p>
-//             <p>Price: {price}</p>
-
-//         </div>
-//     );
-// };
-
-// export default BookDetailsPage;
-// Using generateStaticParams
-//Statically Generate
-export const generateStaticParams = async () => {
-    const res = await fetch('http://localhost:5000/books');
-    const books = await res.json();
-    //[{bookId : 1}, {bookId : 2}]
-    // return books.map(book => book.id)
-    // return books.map(book => ({ bookId: book.id }))
-    //using slice for SSG
-    return books.slice(0, 3).map(book => ({ bookId: book.id }))
-}
+// for dynamic data
+// (Static)   prerendered as static content
+// ƒ  (Dynamic)  server-rendered on demand
+import React from 'react';
 
 const BookDetailsPage = async ({ params }) => {
     const { bookId } = await params;
@@ -44,6 +19,34 @@ const BookDetailsPage = async ({ params }) => {
 };
 
 export default BookDetailsPage;
+// Using generateStaticParams
+//Statically Generate
+// export const generateStaticParams = async () => {
+//     const res = await fetch('http://localhost:5000/books');
+//     const books = await res.json();
+//     //[{bookId : 1}, {bookId : 2}]
+//     // return books.map(book => book.id)
+//     // return books.map(book => ({ bookId: book.id }))
+//     //using slice for SSG
+//     return books.slice(0, 3).map(book => ({ bookId: book.id }))
+// }
+
+// const BookDetailsPage = async ({ params }) => {
+//     const { bookId } = await params;
+//     const res = await fetch(`http://localhost:5000/books/${bookId}`);
+//     const { title, author, price } = await res.json();
+//     return (
+//         <div>
+//             <h2>Book Details : {bookId} </h2>
+//             <h3>{title}</h3>
+//             <p>Author: {author} </p>
+//             <p>Price: {price}</p>
+
+//         </div>
+//     );
+// };
+
+// export default BookDetailsPage;
 // static generate npm run build
 // (SSG)     prerendered as static HTML (uses generateStaticParams)
 // npm start server
